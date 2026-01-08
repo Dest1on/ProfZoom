76a79843c4d37e6462de126933b12b9b5075d544
Dmitry Dem <dimixlive87@gmail.com>
2026-01-08 19:09:08 +0700

    +oper files

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -4,14 +4,20 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/Dest1on/ProfZoom-backend/internal/config"
+	"github.com/Dest1on/ProfZoom-backend/internal/database"
 	apphttp "github.com/Dest1on/ProfZoom-backend/internal/http"
 )
 
 func main() {
+	cfg := config.Load()
+	db := database.NewPostgres(cfg.PostgresDSN)
+	defer db.Close()
+
 	router := apphttp.NewRouter()
 
-	log.Println("API started on :8080")
-	if err := http.ListenAndServe(":8080", router); err != nil {
+	log.Println("API started on :" + cfg.HTTPPort)
+	if err := http.ListenAndServe(":"+cfg.HTTPPort, router); err != nil {
 		log.Fatal(err)
 	}
 }
